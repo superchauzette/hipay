a5ab945c33d1695be73b096cc4107b398d392c9c
kevin tillot <kevin.tillot@gmail.com>
2019-05-15 18:11:55 +0200

    init

---
+++ src/modules/App/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+
+export function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Hipay</header>
+    </div>
+  );
+}
